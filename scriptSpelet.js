cec009a8a8645ad67b558e819b46b33976b22d72
Eric Classon <142800136+EricClasson@users.noreply.github.com>
2023-11-23 07:52:01 +0100

    message

--- scriptSpelet.js
+++ scriptSpelet.js
@@ -8,56 +8,59 @@ const player2 = document.querySelector(`.player2`);
 
 const playPvpBtn = document.querySelector(`.play-game-btn`);
 const playPveBtn = document.querySelector(`.Starta`);
-const btnMotAndra = document.querySelector('.btn-mot-andra');
-const btnMotDator = document.querySelector('.btn-mot-dator');
+const btnMotAndra = document.querySelector(".btn-mot-andra");
+const btnMotDator = document.querySelector(".btn-mot-dator");
 const pvpPage = document.querySelector(`.PVP`);
 const gamePage = document.querySelector(`.memory-game`);
-const sectionMeny = document.querySelector('.section-meny');
-const sectionData = document.querySelector('.section-data');
+const sectionMeny = document.querySelector(".section-meny");
+const sectionData = document.querySelector(".section-data");
 
 const img = document.querySelector(`.hidden1`);
 
-btnMotAndra.addEventListener('click', function () {
-    sectionMeny.classList.add('hidden-meny');
-    pvpPage.classList.remove('hidden-pvp');
+btnMotAndra.addEventListener("click", function () {
+  sectionMeny.classList.add("hidden-meny");
+  pvpPage.classList.remove("hidden-pvp");
 });
 
-btnMotDator.addEventListener('click', function () {
-    sectionMeny.classList.add('hidden-meny');
-    sectionData.classList.remove('hidden-data');
+btnMotDator.addEventListener("click", function () {
+  sectionMeny.classList.add("hidden-meny");
+  sectionData.classList.remove("hidden-data");
 });
 
 playPvpBtn.addEventListener(`click`, function () {
-    player1.textContent = namePlayer1.value;
-    player2.textContent = namePlayer2.value;
-    pvpPage.classList.add(`hidden-pvp`);
-    gamePage.classList.remove(`hidden-game`);
+  player1.textContent = namePlayer1.value;
+  player2.textContent = namePlayer2.value;
+  pvpPage.classList.add(`hidden-pvp`);
+  gamePage.classList.remove(`hidden-game`);
 });
 playPveBtn.addEventListener(`click`, function () {
-    player1.textContent = namePlayer1Data.value;
-    player2.textContent = `Dator`;
-    sectionData.classList.add(`hidden-data`);
-    gamePage.classList.remove(`hidden-game`);
+  player1.textContent = namePlayer1Data.value;
+  player2.textContent = `Dator`;
+  sectionData.classList.add(`hidden-data`);
+  gamePage.classList.remove(`hidden-game`);
 });
 
 let images = [
-    `assets/Bowser_Jr.png`,
-    `assets/Bowser.png`,
-    `assets/Daisy.png`,
-    `assets/DiddyReturns.png`,
-    `assets/Donkey_Kong.png`,
-    `assets/Luigi.png`,
-    `assets/Mario.png`,
-    `assets/Princess_Peach.png`,
-    `assets/Toad.png`,
-    `assets/Waluigi.png`,
-    `assets/Wario.png`,
-    `assets/Yoshi.png`,
+  `assets/Bowser_Jr.png`,
+  `assets/Bowser.png`,
+  `assets/Daisy.png`,
+  `assets/DiddyReturns.png`,
+  `assets/Donkey_Kong.png`,
+  `assets/Luigi.png`,
+  `assets/Mario.png`,
+  `assets/Princess_Peach.png`,
+  `assets/Toad.png`,
+  `assets/Waluigi.png`,
+  `assets/Wario.png`,
+  `assets/Yoshi.png`,
 ];
 
+// alvins kod
 // img.addEventListener(`click`, function () {
-//     img.classList.add(`läggtill`);
-//     let randomIndex = Math.floor(Math.random() * images.length);
-//     let nybild = images[randomIndex];
-//     img.src = nybild;
+//   img.classList.add(`läggtill`);
+//   let randomIndex = Math.floor(Math.random() * images.length);
+//   let nybild = images[randomIndex];
+//   img.src = nybild;
 // });
+
+//kalles kod
